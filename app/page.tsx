fecb1ee570439bc646905823fc894f96567d37c9
Aditya Patwa <adityapatwa.tech@gmail.com>
2024-03-24 12:15:18 +0000

    Toggle Btn Added

--- app/page.tsx
+++ app/page.tsx
@@ -37,6 +37,14 @@ export default function Home() {
               Connect Wallet
             </button>
           </div>
+
+          <div className={styles.toggleDiv}>
+            <button className={styles.toggleBtn}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" fill="white" className="bi bi-list" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+              </svg>
+            </button>
+          </div>
         </header>
 
         <section className={styles.mainSection}>
@@ -56,6 +64,7 @@ export default function Home() {
           </div>
         </section>
       </main>
+
     </>
   );
 }
