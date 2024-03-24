d91065156300de36711777ba3ca023b87aa42bb9
Aditya Patwa <adityapatwa.tech@gmail.com>
2024-03-24 10:57:29 +0000

    Styling Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,52 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { roboto_slab } from "@/app/ui/fonts";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <main className={styles.main}>
+        <header className={styles.header}>
+          <div className={styles.titleDiv}>
+            <h2>
+              <a href="/" className={`${roboto_slab.className} antialiased`}>Lotus Notebook</a>
+            </h2>
+          </div>
+          <nav className={styles.navBar}>
+            <ul className={`${roboto_slab.className} ${styles.navList} antialiased`}>
+              <li className={styles.navItem}>
+                <a href="/" className={styles.navLink}>Home</a>
+              </li>
+              <li className={styles.navItem}>
+                <a href="/" className={styles.navLink}>Products</a>
+              </li>
+              <li className={styles.navItem}>
+                <a href="/" className={styles.navLink}>Resources</a>
+              </li>
+              <li className={styles.navItem}>
+                <a href="/" className={styles.navLink}>Pricing</a>
+              </li>
+              <li className={styles.navItem}>
+                <a href="/" className={styles.navLink}>Blogs</a>
+              </li>
+            </ul>
+          </nav>
+          <div className={styles.walletDiv}>
+            <button className={`${roboto_slab.className} ${styles.walletBtn} antialiased`}>
+              Connect Wallet
+            </button>
+          </div>
+        </header>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <section className={styles.mainSection}>
+          <div>
+            <h1 className={`${roboto_slab.className} ${styles.tagline} antialiased`}>
+              Explore new possibilities with <br />
+              <span>Lotus Notebooks</span>
+            </h1>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Roboto_Slab } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const roboto_slab = Roboto_Slab({subsets: ['latin'], weight: ["900", "700", "100", "200", "500"]});
