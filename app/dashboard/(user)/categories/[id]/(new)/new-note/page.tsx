118bb9f185dcccdafc1f864a5ef9249ce1245f4c
Aditya Patwa <adityapatwa.tech@gmail.com>
2024-03-28 12:42:23 +0000

    Editor SetUp Done

--- app/dashboard/(user)/categories/[id]/(new)/new-note/page.tsx
+++ app/dashboard/(user)/categories/[id]/(new)/new-note/page.tsx
@@ -1,33 +1,52 @@
 "use client";
 import EditorJS from '@editorjs/editorjs';
 import { useRef, useEffect } from 'react';
-import "./page.module.css";
+import styles from '@/app/dashboard/(user)/categories/[id]/(new)/new-note/page.module.css';
+import Header from '@editorjs/header';
+// @ts-ignore
+import List from '@editorjs/list';
+
 
 export default function NewNote({ params }: { params: { id: string } }) {
     const editorRef = useRef<HTMLDivElement | null>(null);
 
-    useEffect(() => {
-        const editor = new EditorJS({
-            /**
-             * Id of Element that should contain Editor instance
-             */
-            holder: editorRef.current!,
-            
-        });
-    });
-    
+    const isReady = useRef(false);
 
-      
+    useEffect(() => {
+        if (!isReady.current) {
+            const editor = new EditorJS({
+                /**
+                 * Id of Element that should contain Editor instance
+                 */
+                holder: editorRef.current!,
+                placeholder: "Let's Start😇....",
+
+
+
+                /** 
+   * Available Tools list. 
+   * Pass Tool's class or Settings object for each Tool you want to use 
+   */
+                tools: {
+                    header: Header,
+                    list: List
+                },
+            });
+            isReady.current = true;
+        }
+    }, []);
+
+
+
 
     return (
         <>
-        <h1>
-            Creating New Note <br />
-            Under category of Id: {params.id}
-        </h1>
+            <h1 className={styles.title}>
+                New Note ✒️
+            </h1>
 
 
-            <section ref={editorRef}>
+            <section className={styles.editor} ref={editorRef}>
 
             </section>
         </>
