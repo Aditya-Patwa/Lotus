04651e7d87aa1dc1b7d0d7017a6ae4e14eb86548
Aditya Patwa <adityapatwa.tech@gmail.com>
2024-03-30 12:47:43 +0000

    Database Completed for Notes and CreateNote web3 backend done

--- app/dashboard/(user)/categories/[id]/(new)/new-note/page.tsx
+++ app/dashboard/(user)/categories/[id]/(new)/new-note/page.tsx
@@ -26,6 +26,14 @@ import { useAnchorWallet } from "@solana/wallet-adapter-react";
 import { Connection, PublicKey, SystemProgram, Keypair } from "@solana/web3.js";
 import { Program, AnchorProvider } from "@project-serum/anchor";
 import { useWallet } from "@solana/wallet-adapter-react";
+import { idl } from '@/app/idl';
+
+
+const programID = new PublicKey("2T8nS5g6szDurxKHS2RGwr1ve9MBhxyX73t7HsLFFBAs");
+const network = "http://127.0.0.1:8899"; // Adjust for your environment: local, devnet, or mainnet-beta
+const opts = { preflightCommitment: "processed" };
+
+
 
 
 export default function NewNote({ params }: { params: { id: string } }) {
@@ -36,14 +44,50 @@ export default function NewNote({ params }: { params: { id: string } }) {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
 
+    const wallet = useAnchorWallet();
+    const { connected, publicKey } = useWallet();
 
-    
 
+    const getProvider = () => {
+        if (!wallet) return null;
+        const connection = new Connection(network, opts.preflightCommitment);
+        return new AnchorProvider(connection, wallet, opts.preflightCommitment);
+    };
+
+    const createGreeting = async () => {
+        const provider = getProvider();
+        if (!provider) {
+            setError("Provider is not available.");
+            return;
+        }
+
+        const program = new Program(idl, programID, provider);
+
+        try {
+            const newAccountKp = Keypair.generate();
+
+            console.log(newAccountKp.publicKey.toString());
+
+            const txHash = await program.methods
+                .createNote()
+                .accounts({
+                    noteAccount: newAccountKp.publicKey,
+                    signer: publicKey,
+                    systemProgram: SystemProgram.programId,
+                })
+                .signers([newAccountKp])
+                .rpc();
+
+        } catch (err) {
+            console.error("Error creating greeting account:", err);
+            setError("Failed to create greeting account. Please try again.");
+        }
+    }
 
 
     function saveNote() {
         console.log(title, description);
-        if(title.trim().length === 0) {
+        if (title.trim().length === 0) {
             console.log("Title is Empty");
             setError("Title cannot be empty ❌");
             return;
@@ -55,19 +99,56 @@ export default function NewNote({ params }: { params: { id: string } }) {
             return;
         }
 
+        const provider = getProvider();
+        if (!provider) {
+            setError("Provider is not available.");
+            return;
+        }
+
+
+        const program = new Program(idl, programID, provider);
+
         editorJsRef.current.save().then(async (outputData) => {
             console.log('Article data: ', outputData);
-            if(outputData.blocks.length === 0) {
+            if (outputData.blocks.length === 0) {
                 console.log("No Data feeded");
                 setError("Blog Post cannot be empty ❌");
                 return;
             } else {
                 setError(null);
 
-                let res = await fetch("/api/savepost", {method: "POST", headers: {'Content-Type': 'application/json'}, body: JSON.stringify({title: title, description: description, data: outputData})});
+                let res = await fetch("/api/savepost", { method: "POST", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ title: title, description: description, data: outputData }) });
                 let data = await res.json();
 
+                console.log(data.blob.url);
 
+                try {
+                    const noteAccount = Keypair.generate();
+                    const txHash = await program.methods
+                        .createNote(title, description, data.blob.url, null, publicKey)
+                        .accounts({
+                            noteAccount: noteAccount.publicKey,
+                            signer: publicKey,
+                            systemProgram: SystemProgram.programId,
+                        })
+                        .signers([noteAccount])
+                        .rpc();
+
+                    console.log(txHash);
+
+
+                    // const latestBlockHash = await provider.connection.getLatestBlockhash();
+                    // // Confirm transaction
+
+                    // await provider.connection.confirmTransaction({
+                    //     blockhash: latestBlockHash.blockhash,
+                    //     lastValidBlockHeight: latestBlockHash.lastValidBlockHeight, signature: txHash
+                    // });
+
+                } catch (err) {
+                    console.error("Error creating greeting account:", err);
+                    setError("Failed to create greeting account. Please try again.");
+                }
 
             }
         }).catch((error) => {
