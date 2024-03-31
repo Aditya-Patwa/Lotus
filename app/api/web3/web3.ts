c86903dc044ccfff98396ad7f93de8de03b11cc0
Aditya Patwa <adityapatwa.tech@gmail.com>
2024-03-31 12:18:20 +0000

    Error Fixed

--- app/api/web3/web3.ts
+++ app/api/web3/web3.ts
@@ -1,6 +1,6 @@
 import { Connection, PublicKey, SystemProgram, Keypair, ConfirmOptions } from "@solana/web3.js";
 
 
-export const programID = new PublicKey(process.env.PROGRAM_ID!);
+export const programID = new PublicKey("2T8nS5g6szDurxKHS2RGwr1ve9MBhxyX73t7HsLFFBAs");
 export const network = "https://api.devnet.solana.com"; // Adjust for your environment: local, devnet, or mainnet-beta
 export const opts: ConfirmOptions = { preflightCommitment: "processed" };
